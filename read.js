87114655b974c037b0073a2decda4001f614342d
creeperkafasipw <49831545+creeperkafasi@users.noreply.github.com>
2020-08-10 21:15:15 +0300

    text page input

--- read.js
+++ read.js
@@ -4,19 +4,28 @@ var pageNum = 1;
 
 function episode(selector) {
     epNum = selector.options.selectedIndex + 1;
-    console.log("Switched to episode " + epNum);
     document.getElementById("page").src = "episodes/"+epNum+"/1.png";
     document.getElementById("comment").src = "episodes/"+epNum+"/1c.png";
+    console.log("Switched to episode " + epNum);
 }
 
 function prev(){
     pageNum--;
     document.getElementById("page").src = "episodes/"+epNum+"/"+pageNum+".png";
     document.getElementById("page").title = "Page " + pageNum;
+    console.log("Switched to page " + pageNum);
 }
 
 function next(){
     pageNum++;
     document.getElementById("page").src = "episodes/"+epNum+"/"+pageNum+".png";
     document.getElementById("page").title = "Page " + pageNum;
+    console.log("Switched to page " + pageNum);
+}
+
+function page(input){
+    pageNum = input.value;    
+    document.getElementById("page").src = "episodes/"+epNum+"/"+pageNum+".png";
+    document.getElementById("page").title = "Page " + pageNum;
+    console.log("Switched to page " + pageNum);
 }
