b872a62947dff52052ef77dd510a82e6141e0517
creeperkafasipw <49831545+creeperkafasi@users.noreply.github.com>
2020-08-10 16:14:29 +0300

    progress

---
+++ script.js
@@ -0,0 +1,34 @@
+var MouseX;
+var MouseY;
+
+
+window.addEventListener("mousemove",function(e) {
+    MouseX=e.clientX;
+    MouseY=e.clientY;
+});
+
+var height;
+function anim1() {
+    var interval = setInterval(frame, 5);
+    height=0;
+    function frame() {
+        if (document.getElementById("navbar").style.height == "40px") {
+        
+        } else {
+            document.getElementById("navbar").style.height = String(height) + "px";
+            height += 0.5;
+        }
+    }
+}
+
+
+function show(element){
+    document.getElementById("navbar").style.visibility = "visible";
+    document.getElementById("navbar").style.height = "30px";
+    
+}
+
+function hide(element) {
+    document.getElementById("navbar").style.visibility = "hidden";
+    document.getElementById("navbar").style.height = "0px";
+}
